6618ae1f987037bc38181f44d03c5511429704fd
lusk <lucas78654321@outlook.com>
2023-03-26 20:54:31 -0300

    feat: add props for todo component

--- src/components/Todo/index.tsx
+++ src/components/Todo/index.tsx
@@ -2,22 +2,27 @@ import { useState } from 'react';
 import { Checkbox } from '../CheckBox';
 import { Cross } from '../Cross';
 
-interface todoProps {}
+interface todoProps {
+  name: string,
+  check: boolean
+}
 
-export const Todo = () => {
+export const Todo = ({check, name}: todoProps) => {
   const [checked, setChecked] = useState(false);
 
   const handleCheckboxChange = () => {
+    console.log(check)
+    check = checked
     setChecked(!checked);
   };
   return (
-    <div className='flex min-w-[338px] items-center	justify-between border-b-2 border-zinc-900 bg-gray-700 px-4 py-[18px] hover:cursor-pointer'>
+    <div className='flex items-center	justify-between border-b-2 border-zinc-900 bg-gray-700 px-4 py-[18px] hover:cursor-pointer'>
       <label
         htmlFor=''
         className='flex items-center gap-2 hover:cursor-pointer'
       >
         <Checkbox checked={checked} onChange={handleCheckboxChange} />
-        <span className='text-white-500'>Create a new todo...</span>
+        <span className='text-white-500'>{name}</span>
       </label>
       <Cross />
     </div>
