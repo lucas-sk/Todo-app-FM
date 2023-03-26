44e66a1fd88392ddd46e9e0c908cd60a2fcd1f88
lusk <lucas78654321@outlook.com>
2023-03-26 20:03:40 -0300

    feat: add props checkbox component

--- src/components/CheckBox/index.tsx
+++ src/components/CheckBox/index.tsx
@@ -1,22 +1,18 @@
-import { useState } from "react";
+import { InputHTMLAttributes } from "react";
 import { Check } from '../Check';
 
-export const Checkbox = () => {
-  const [checked, setChecked] = useState(false);
+interface CheckboxProps extends InputHTMLAttributes<HTMLInputElement>{}
 
-  const handleCheckboxChange = () => {
-    setChecked(!checked);
-  };
+export const Checkbox = (props: CheckboxProps) => {
   return (
     <>
       <input
         id="checkbox"
         type="checkbox"
-        className="h-[18px] w-[18px] border appearance-none checked:bg-gradient-to-b from-initial-500  to-end-500  border-zinc-900 rounded-full"
-        checked={checked}
-        onChange={handleCheckboxChange}
+        className="checkbox relative right-0 left-0 top-0 bottom-0 h-[18px] w-[18px] border appearance-none checked:bg-gradient-to-b checked:from-initial-500  checked:to-end-500 border-zinc-900 rounded-full checked:border-none "
+        {...props}
       />
-      {!!checked && <Check className="absolute left-1  top-[5px]"/>}
+      {!!props.checked && <Check className="absolute left-5 right-0"/>}
     </>
   );
 }
