0f8de022792c6c71d62f8859b9a896818702be83
lusk <lucas78654321@outlook.com>
2023-04-04 07:33:05 -0300

    feat: add onClick and onChecked on todo

--- src/components/Todo/index.tsx
+++ src/components/Todo/index.tsx
@@ -1,30 +1,28 @@
-import { useState } from 'react';
+import { Button } from '../Button';
 import { Checkbox } from '../CheckBox';
 import { Cross } from '../Cross';
 
 interface todoProps {
   name: string,
-  check: boolean
+  check: boolean,
+  onClick(): void
+  onChecked(): void
 }
 
-export const Todo = ({check, name}: todoProps) => {
-  const [checked, setChecked] = useState(false);
+export const Todo = ({check, name, onClick, onChecked}: todoProps) => {
 
-  const handleCheckboxChange = () => {
-    console.log(check)
-    check = checked
-    setChecked(!checked);
-  };
   return (
     <div className='flex items-center	justify-between border-b-2 border-zinc-900 bg-gray-700 px-4 py-[18px] hover:cursor-pointer'>
       <label
         htmlFor=''
         className='flex items-center gap-2 hover:cursor-pointer'
       >
-        <Checkbox checked={checked} onChange={handleCheckboxChange} />
+        <Checkbox checked={check} onChange={onChecked} />
         <span className='text-white-500'>{name}</span>
       </label>
-      <Cross />
+      <Button onClick={onClick}>
+        <Cross />
+      </Button>
     </div>
   );
 };
