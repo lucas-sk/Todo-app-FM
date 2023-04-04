531e715f62c0fb544615acd52128d02769b03d5a
lusk <lucas78654321@outlook.com>
2023-04-03 21:01:10 -0300

    feat: implement remove and add todo

--- src/App.tsx
+++ src/App.tsx
@@ -1,41 +1,66 @@
-import { useState } from 'react';
+import { ChangeEvent, KeyboardEvent, useState } from 'react';
 import { Header } from './components/Header';
 import { Input } from './components/Input';
 import { Picture } from './components/Picture';
 import { Todo } from './components/Todo';
 import { todo } from './interface/todo';
 
+export const listTodos: todo[] = [
+  { id: '1', name: 'todo', check: true },
+  { id: '2', name: 'todo', check: false },
+  { id: '3', name: 'todo', check: false },
+  { id: '4', name: 'todo', check: false },
+  { id: '5', name: 'todo', check: false },
+  { id: '6', name: 'todo', check: false },
+];
+
 export const App = () => {
-  const [todos, setTodos] = useState<todo[]>([]);
+  const [name, setName] = useState('')
+  const [todos, setTodos] = useState<todo[]>(listTodos);
+
+  const handleNameTodo = (e: ChangeEvent<HTMLInputElement>) => {
+    setName(e.target.value)
+  }
+
+   function handleAddTodo(event: KeyboardEvent<HTMLInputElement>) {
+    if (event.key === "Enter") {
+      console.log('Enter pressionado!');
+      const todo: todo =  {
+      name: name,
+      check: false,
+      id: `${todos.length + 1}`
+    }
+    setTodos([...todos, todo]);
+    setName("");
+    }
+  }
 
 
-  const listTodos: todo[] = [
-    { id: '1', name: 'todo', check: true },
-    { id: '2', name: 'todo', check: false },
-    { id: '3', name: 'todo', check: false },
-    { id: '4', name: 'todo', check: false },
-    { id: '5', name: 'todo', check: false },
-    { id: '6', name: 'todo', check: false },
-  ];
+  const handleRemoveTodo = (id: string): any => {
+    const newList = todos.filter((todo) => todo.id !== id ? todo : null)
+    setTodos(newList)
+  }
+
+
 
   return (
-    <div className='min-h-screen w-screen flex-col gap-1 bg-gray-900'>
+    <div className='min-h-screen flex flex-col items-center max-w-full	w-screen bg-gray-900'>
       <Picture />
-      <div className='z-1 relative  mx-6 flex flex-col gap-10 pt-12'>
+      <div className='z-1 relative max-w-lg w-80 mx-6 h-auto flex flex-col gap-10 pt-12'>
         <Header />
         <div className='flex flex-col gap-4'>
-          <Input />
+          <Input onChange={handleNameTodo} value={name} onKeyDown={handleAddTodo} />
           <div className=''>
           <>
-            {listTodos.length > 0 && listTodos.map((todo) => {
+            {todos.length > 0 && todos.map((todo) => {
               return (
                 <>
-                  <Todo key={todo.id} check={todo.check} name={todo.name}/>
+                  <Todo key={todo.id} check={todo.check} name={todo.name} onClick={() => handleRemoveTodo(todo.id)}/>
                 </>
                 )
               })}
               <div className='flex px-4 py-[18px] justify-between bg-gray-700 w-full text-zinc-500'>
-                <p>{listTodos.length} Items left</p>
+                <p>{todos.length} Items left</p>
                 <p>Clear Completed</p>
               </div>
           </>
@@ -51,5 +76,6 @@ export const App = () => {
         </p>
       </div>
     </div>
+
   );
 };
