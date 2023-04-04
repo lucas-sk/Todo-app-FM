7514785c805adac2d0d8158341db9e7eaa65f9ac
lusk <lucas78654321@outlook.com>
2023-04-03 21:00:28 -0300

    style(Header): add align items center

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -2,7 +2,7 @@ import Sun from '../Sun';
 
 export const Header = () => {
   return (
-    <header className='flex justify-between '>
+    <header className='flex justify-between items-center'>
       <h3 className='text-[1.625rem] font-bold  tracking-[0.4em] text-white-700'>
         TODO
       </h3>
